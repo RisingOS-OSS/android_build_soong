bce87d389af3c0a5cf65dbdb2bead8ac4c0d6b58
minaripenguin <minaripenguin@users.noreply.github.com>
2024-10-30 16:30:06 +0800

    [RisingOS] Add chipset sysprop

--- scripts/gen_build_prop.py
+++ scripts/gen_build_prop.py
@@ -118,6 +118,7 @@ def parse_args():
   config["LineageDesc"] = config["BuildDesc"]
   config["LineageDevice"] = config["DeviceName"]
   
+  config["RisingChipset"] = ""
   config["RisingMaintainer"] = ""
 
   if config["BuildNumber"].startswith("eng."):
@@ -254,6 +255,7 @@ def generate_build_info(args):
 
   print(f"ro.lineage.device={config['LineageDevice']}")
   
+  print(f"ro.rising.chipset={config['RisingChipset']}")
   print(f"ro.rising.maintainer={config['RisingMaintainer']}")
   
   print(f"persist.sys.pihooks_FINGERPRINT={config['PihooksGmsFp']}")
